8d9edb3775f5f607029b45b8bbc541a3afb1cd75
ashhhlynn <ashleylynnwelch@gmail.com>
2022-06-02 17:32:31 -0500

    commit tree 647ed368ba84da18e7b5a3cf24755d0e7a5edafa

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+    get '/' do
+      erb :user_input 
+    end
+  
+    post '/piglatinize' do
+      input_from_user = params[:word]
+      @pig_latinizer = PigLatinizer.new(input_from_user)
+    
+    
+    end
+    
+  end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,15 @@
+class PigLatinizer
+    attr_reader :word
+  
+    def initialize(word)
+      @word = word 
+    end
+
+    def split_sentence(word)
+        split_words = words.split(" ")
+    end
+
+    def piglatinize
+    end 
+  
+end
